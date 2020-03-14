430946546886f63a9ac112cc6f7ba85d59d20569
1000ship <cjstjdgur1234@gmail.com>
2020-03-14 16:19:13 +0900

    Before changing root path

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from "Components/Header";
-import Router from "Components/Router";
+import Header from "./Components/Header";
+import Router from "./Components/Router";
 
 function App() {
   return (
